05773a33c5edc15fe51b0af7008b95eb1d9731a4
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2026-01-28 16:17:29 +0530

    completed basics structure for progress bar

---
+++ progress_bar/src/ProgressBar.jsx
@@ -0,0 +1,13 @@
+function ProgressBar({ progress }) {
+  return (
+    <>
+      <div className="outer">
+        <div className="inner" style={{ width: `${progress}%` }}>
+          {progress}%
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ProgressBar;
